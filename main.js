5aaf64fa22b3a80b4d7966dd35b2a5d3b4ab4f24
Bassim Durzi <bassim.ali.durzi@gmail.com>
2024-05-08 00:48:21 +0200

    Done!

--- main.js
+++ main.js
@@ -1,31 +1,63 @@
-console.log(getComputerChoice());
-console.log(getHumanChoice());
+// Rock Paper Scissors
+console.log("Rock Paper Scissors");
 
+// Random choise by computer
 function getComputerChoice() {
-  randomNumber = Math.floor(Math.random() * 3 + 1);
-  let randomChoise;
-  console.log(randomNumber);
+  let choiceNumber = Math.floor(Math.random() * 3 + 1);
 
-  switch (randomNumber) {
-    case 1:
-      randomChoise = "Rock";
-      console.log(randomChoise);
-      break;
+  let computerChoice =
+    choiceNumber === 1 ? "rock" : choiceNumber === 2 ? "paper" : "scissors";
+  return computerChoice;
+}
 
-    case 2:
-      randomChoise = "Paper";
-      console.log(randomChoise);
-      break;
-
-    case 3:
-      randomChoise = "Scissors";
-      console.log(randomChoise);
-      break;
+// Human choice
+function getHumanChoice() {
+  let humanChoice = prompt("Please choose Rock, Paper or Scissors");
+  if (humanChoice !== null) {
+    if (
+      humanChoice.toLowerCase() === "rock" ||
+      humanChoice.toLowerCase() === "paper" ||
+      humanChoice.toLowerCase() === "scissors"
+    ) {
+      return humanChoice.toLocaleLowerCase();
+    } else {
+      console.log("Your choice does not match the choices in this game!");
+    }
   }
 }
 
-function getHumanChoice() {
-    let humanChoise = prompt ('Please enter your choise Rock, Paper or Scissors')
-    console.log('You have choosed:' + humanChoise);
+//Play Round
+
+let computerScore = 0;
+let humanScore = 0;
+
+let computerSelection = getComputerChoice();
+let humanSelection = getHumanChoice();
+
+function PlayRound(humanSelection, computerSelection) {
+  if (humanSelection) {
+    let result =
+      humanSelection === computerSelection
+        ? "Game Tie!"
+        : (humanSelection === "rock" && computerSelection === "scissors") ||
+          (humanSelection === "scissors" && computerSelection === "paper") ||
+          (humanSelection === "paper" && computerSelection === "rock")
+        ? (humanScore = 1)
+        : (computerScore = 1);
+
+    console.log("Your choice: " + humanSelection);
+    console.log("Computer Choice: " + computerSelection);
+
+    if (humanScore === 1) {
+      console.log("You wins this time and get 1 point!");
+    } else if (computerScore === 1) {
+      console.log("Computer wins this time and get 1 point!");
+    } else {
+      console.log("Game tie. No one gets a point.");
+    }
+  } else {
+    console.log("You should enter a choice!");
+  }
 }
+PlayRound(humanSelection, computerSelection);
 
